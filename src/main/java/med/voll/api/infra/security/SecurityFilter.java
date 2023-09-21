949d91ecf81c05d882a25ee66f4b5c979c20cdac
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2023-09-21 18:43:21 -0500

    hasta obteniendo token#1

---
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -0,0 +1,26 @@
+package med.voll.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("El filtro esta siendo llamado");
+        // obtener el token del header
+        var token=request.getHeader("Authorization");
+        if (token==null || token==""){
+            throw new RuntimeException("El token enviado no es valido");
+        }
+        token=token.replace("Bearer ", "");
+        System.out.println("token "+token);
+        filterChain.doFilter(request,response);
+    }
+}
--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -33,8 +33,4 @@ public class TokenService {
     private Instant generarFechaExpiracion() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-05:00"));
     }
-//
-//    public String getApiSecret() {
-//        return apiSecret;
-//    }
 }
